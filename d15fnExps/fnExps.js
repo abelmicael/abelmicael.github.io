58e675e5f0781516f96d36099c84c6be43e51cf8
abelmicael <amicael@miu.edu>
2021-09-05 20:12:40 -0500

    Assignments

--- d15fnExps/fnExps.js
+++ d15fnExps/fnExps.js
@@ -1,32 +1,36 @@
 "use strict";
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser
-*/
-//module.exports = { double, times100, myMap}; 
-//add all of your function names here that you need for the node mocha tests
+ */
+// module.exports = { double, times100, myMap }; //add all of your function names here that you need for the node mocha tests
 
 /**
- *@returns {number} double the input
+ * @param {number} num input number
+ *@returns {number}  double the input
  */
-function double(number) {
-    return number * 2;
+function double(num) {
+    return num * 2;
 }
 
+
 /**
+ * @param {number} num input number
  * @returns {number} 100 times the input
  */
-function times100(number) {
-    return number * 100;
+function times100(num) {
+    return num * 100;
 }
 
+
 /**
+ * @param {Array} arr input
+ * @param {function} func input
  * @returns {Array} creates a new array with function mapped to each element
  */
 function myMap(arr, func) {
-    const arr1 = [];
+    let newArray = [];
     for (let i = 0; i < arr.length; i++) {
-        arr1[i] = func(arr[i])
+        newArray[i] = func(arr[i]);
     }
-    return arr1;
+    return newArray;
 }
-
