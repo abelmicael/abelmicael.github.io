771884df416edf0cfe68136a578359169b466868
abelmicael <amicael@miu.edu>
2021-09-15 22:09:28 -0500

    Assignment

--- d31Recursion/recursion.js
+++ d31Recursion/recursion.js
@@ -6,19 +6,31 @@
 //module.exports = { sumTo, factorial, fibonacci, outputList, outputListLoop, reverseList, reverseListLoop }
 //add all of your function names here that you need for the node mocha tests
 
-
+/**
+ * 
+ * @param {number} num any number to be summed recursivley
+ * @returns {number} returns sum
+ */
 function sumTo(num) {
     if (num === 0) return 0;
 
     return num + sumTo(num - 1);
 }
-
+/**
+ * 
+ * @param {Number} n any number to be multiplied
+ * @returns {Number} result
+ */
 function factorial(n) {
     if (n === 0) return 1;
 
     return n * factorial(n - 1);
 }
-
+/**
+ * 
+ * @param {Number} n any number
+ * @returns {Number} returns the fibonacci result of the number
+ */
 function fibonacci(n) {
     if (n === 0) {
         return 0;
@@ -29,13 +41,21 @@ function fibonacci(n) {
         return fibonacci(n - 2) + fibonacci(n - 1);
     }
 }
-
+/**
+ * 
+ * @param {Node} list any node
+ * @returns {String} returns concated values of the nodes in the list
+ */
 function outputList(list) {
     if (list.next == null) return list.value + " printed to console";
 
     return list.value + " " + outputList(list.next);
 }
-
+/**
+ * 
+ * @param {Node} list any list of node
+ * @return {String} returns concated values of the nodes in the list
+ */
 function outputListLoop(list) {
 
     let str = "";
@@ -46,14 +66,22 @@ function outputListLoop(list) {
     }
     return str + list.value + " printed to console"
 }
-
+/**
+ * 
+ * @param {Node} any list of node
+ * @returns {String} returns concated reversed values of the nodes in the list
+ */
 function reverseList(list) {
 
     if (list.next === null) return list.value;
 
     return reverseList(list.next) + " " + list.value;
 }
-
+/**
+ * 
+ * @param {Node} any list of node
+ * @returns {String} returns concated reversed values of the nodes in the list
+ */
 function reverseListLoop(list) {
 
     let str = "";
--- d32recursiveStructures/spread.js
+++ d32recursiveStructures/spread.js
@@ -5,20 +5,38 @@
 //module.exports = {copyArray, concat, findMin, combineObjs }; //add all of your function names here that you need for the node mocha tests
 
 
-
+/**
+ * 
+ * @param {Array} arr array to copy its elements
+ * @returns {Array} returns array of elements
+ */
 function copyArray(arr) {
     const newArr = [...arr];
     return newArr;
 }
-
+/**
+ * 
+ * @param {Array} arr1 array of elements
+ * @param {Array} arr2 array of elements
+ * @returns {Array} returns concated elements of arr1 and arr2
+ */
 function concat(arr1, arr2) {
     return arr1.concat(arr2)
 }
-
+/**
+ * 
+ * @param  {...any} numbers any numbers
+ * @returns {number} returns minimum number
+ */
 function findMin(...numbers) {
     return Math.min(...numbers);
 }
-
+/**
+ * 
+ * @param {Object} obj1 object
+ * @param {Object} obj2 object
+ * @returns {Object} retuns object
+ */
 function combineObjs(obj1, obj2) {
     const merge={...obj1,...obj2}
     return merge
